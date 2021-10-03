ff7dfde0a8218f4a01f82612aab775db8e3624f3
chap4ev <sergey.02.nesterenko@gmail.com>
2021-10-03 22:57:36 +0300

    main.cpp fix

--- homework/hw_1/src/main.cpp
+++ homework/hw_1/src/main.cpp
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// main.cpp - содержит главную функцию, 
+// main.cpp - содержит главную функцию,
 // обеспечивающую простое тестирование
 //------------------------------------------------------------------------------
 
@@ -12,65 +12,63 @@
 #include "container.h"
 
 void errMessage1() {
-    cout << "incorrect command line!\n"
-            "  Waited:\n"
-            "     command -f infile outfile01 outfile02\n"
-            "  Or:\n"
-            "     command -n number outfile01 outfile02\n";
+    std::cout << "incorrect command line!\n"
+              "  Waited:\n"
+              "     command -f infile outfile01 outfile02\n"
+              "  Or:\n"
+              "     command -n number outfile01 outfile02\n";
 }
 
 void errMessage2() {
-    cout << "incorrect qualifier value!\n"
-            "  Waited:\n"
-            "     command -f infile outfile01 outfile02\n"
-            "  Or:\n"
-            "     command -n number outfile01 outfile02\n";
+    std::cout << "incorrect qualifier value!\n"
+              "  Waited:\n"
+              "     command -f infile outfile01 outfile02\n"
+              "  Or:\n"
+              "     command -n number outfile01 outfile02\n";
 }
 
 //------------------------------------------------------------------------------
 int main(int argc, char* argv[]) {
-    if(argc != 5) {
+    if (argc != 5) {
         errMessage1();
         return 1;
     }
 
-    cout << "Start"<< endl;
+    std::cout << "Start" << std::endl;
     container c;
     Init(c);
 
     ////cout << "argv[1] = " << argv[1] << "\n";
-    if(!strcmp(argv[1], "-f")) {
-        ifstream ifst(argv[2]);
+    if (!strcmp(argv[1], "-f")) {
+        std::ifstream ifst(argv[2]);
         In(c, ifst);
-    }
-    else if(!strcmp(argv[1], "-n")) {
+    } else if (!strcmp(argv[1], "-n")) {
         auto size = atoi(argv[2]);
-        if((size < 1) || (size > 10000)) { 
-            cout << "incorrect numer of figures = "
-                 << size
-                 << ". Set 0 < number <= 10000\n";
+        if ((size < 1) || (size > 10000)) {
+            std::cout << "incorrect numer of figures = "
+                      << size
+                      << ". Set 0 < number <= 10000\n";
             return 3;
         }
         // системные часы в качестве инициализатора
         srand(static_cast<unsigned int>(time(0)));
         // Заполнение контейнера генератором случайных чисел
         InRnd(c, size);
-    }
-    else {
+    } else {
         errMessage2();
         return 2;
     }
 
     // Вывод содержимого контейнера в файл
-    ofstream ofst1(argv[3]);
+    std::ofstream ofst1(argv[3]);
     ofst1 << "Filled container:\n";
     Out(c, ofst1);
 
-    // The 2nd part of task
-    ofstream ofst2(argv[4]);
-    ofst2 << "Perimeter sum = " << PerimeterSum(c) << "\n";
+    // // The 2nd part of task
+    // std::ofstream ofst2(argv[4]);
+    // ofst2 << "Perimeter sum = " << PerimeterSum(c) << "\n";
 
     Clear(c);
-    cout << "Stop"<< endl;
+    std::cout << "Stop" << std::endl;
     return 0;
 }
